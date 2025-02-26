3f3ee71a8980c8b866f0ab2b90b70fc0c0629a78
Никита Кулагин <nikitakulagin@MacBook-Pro-Nikita.local>
2025-02-26 16:54:46 +0300

    Add gh-pages configs

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import MainGamePage from './components/MainGamePage';
 
-/**
- * Файл App.js
- * Настраивает маршруты с помощью react-router-dom (v6).
- * Вместо Switch используем <Routes>, а вместо component= используем element=.
- */
 function App() {
   return (
-    <Router>
+    // Заменяем BrowserRouter на HashRouter
+    <HashRouter>
       <Routes>
-        {/* Стартовая страница по адресу "/" */}
         <Route path="/" element={<LandingPage />} />
-        {/* Основная страница игры по адресу "/game" */}
         <Route path="/game" element={<MainGamePage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
